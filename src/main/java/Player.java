7b662b134c9ed42a21fa7cee3448c5a0f8a88619
Jocelyn Harper <Josie@Zipcoder01s-MacBook-Pro.local>
2016-01-29 17:19:26 -0500

    Update on Player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,5 +1,46 @@
 /**
  * Created by wscown on 1/29/16.
  */
+import java.util.*;
+
 public class Player {
+
+
+    private String name;
+    private int score;
+
+
+    private void Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+
+    private ArrayList<Card> hand = new ArrayList<Card>(
+
+    );
+
+    public ArrayList<Card> printHand() {
+
+        if (!hand.isEmpty()) {
+            return hand;
+        } else {
+            return null;
+        }
+    }
+
+    public void setCard(Card card){
+
+
+
+    }
+
+    public void getCard(int Position){
+
+        return ;
+
+    }
+
+
+
+
 }
